9f7039a921328a36e1ad14cd65e462efef77ad30
aCertainProgrammer <a_certain_programmer@proton.me>
2025-07-28 13:38:28 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vite.dev/config/
+export default defineConfig({
+	server: {
+		port: 8080,
+		host: "localhost",
+		open: true,
+		strictPort: false,
+	},
+	plugins: [svelte()],
+});
