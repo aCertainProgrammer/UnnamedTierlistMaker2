4ecb43b23dd9435a2d13355476c67446b4633762
aCertainProgrammer <a_certain_programmer@proton.me>
2025-09-11 15:26:15 +0200

    add patch image update script

---
+++ scripts/updatePatch.ts
@@ -0,0 +1,90 @@
+import { exec } from "node:child_process";
+import { captureRejections } from "node:events";
+import { createWriteStream } from "node:fs";
+import { writeFile } from "node:fs/promises";
+import { stdout } from "node:process";
+import { finished, pipeline, Readable } from "node:stream";
+
+type PatchData = {
+	n: {
+		champion: String;
+	};
+};
+
+const patch_json: PatchData = await fetch(
+	"https://ddragon.leagueoflegends.com/realms/euw.json",
+).then((data) => data.json());
+const patchString = patch_json.n.champion;
+
+const smallApiPath = `https://ddragon.leagueoflegends.com/cdn/${patchString}/img/champion/`;
+const smallFsPath = "../public/img/champion/";
+const smallPostfix = ".png";
+
+const champions = await fetch(
+	`https://ddragon.leagueoflegends.com/cdn/${patchString}/data/en_US/champion.json`,
+)
+	.then((data) => data.json())
+	.then((json) => json.data);
+
+async function processChampion(champion: String, i: number) {
+	console.log(
+		"Working on " +
+			champion +
+			" - " +
+			i +
+			"/" +
+			Object.keys(champions).length,
+	);
+
+	const smallChampionResponse = await fetch(
+		smallApiPath + champion + smallPostfix,
+	);
+
+	if (smallChampionResponse == null || smallChampionResponse.body == null) {
+		console.error("things are null: " + champion);
+		return;
+	}
+
+	await writeFile(
+		smallFsPath + champion + smallPostfix,
+		Readable.fromWeb(smallChampionResponse.body),
+		{
+			flag: "w",
+		},
+	);
+
+	console.log(
+		"Done with " +
+			champion +
+			" - " +
+			i +
+			"/" +
+			Object.keys(champions).length,
+	);
+}
+
+const promises: Array<Promise<void>> = [];
+let i = 1;
+for (let champion in champions) {
+	const promise = processChampion(champion, i);
+	promises.push(promise);
+
+	i += 1;
+}
+
+await Promise.all(promises);
+
+exec(
+	"cd ../public/img/champion_icons/ && python3 ./image_minifier.py",
+	(err, stdout, stderr) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		console.log(stdout);
+	},
+);
+
+// this is just to make the file an ES6 Module
+export {};
